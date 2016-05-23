dfd0ef037d1f25c7a31542c512da2c0a9f4ba8ad
shahmohamed <shahmohamed>
2016-05-22 20:21:50 -0400

    Added typings

---
+++ typings/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="./main.d.ts" />
+/// <reference path="../node_modules/reflect-metadata/reflect-metadata.d.ts"/>
+/// <reference path="../node_modules/inversify-dts/inversify/inversify.d.ts" />
